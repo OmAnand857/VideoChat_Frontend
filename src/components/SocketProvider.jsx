1ee648c01eb66499c3d3387c738dbbdbc55ff460
OmAnand857 <142936150+OmAnand857@users.noreply.github.com>
2024-03-31 01:03:39 +0530

    Update SocketProvider.jsx

--- src/components/SocketProvider.jsx
+++ src/components/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const SocketContext = createContext(null);
 
 export const SocketProvider = (props) =>{
             
-            const socket = useMemo(()=>io('https://server-backend-two.vercel.app'),[]) ;
+            const socket = useMemo(()=>io('https://server-backend-p3ey.vercel.app'),[]) ;
 
     return (<SocketContext.Provider value={socket}>
         {props.children}
